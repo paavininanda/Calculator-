f71381879165a9eb6532b0b1ca6df3e6cc5f9bc9
Nupur Baghel <nupurbaghel@gmail.com>
2017-10-05 00:25:59 +0530

    corrected infinite error

--- calculator.js
+++ calculator.js
@@ -55,7 +55,7 @@ function calc() {
         var num1=0;
         var num2=0;
         var i=0;
-        while(textt[i]!='+' && textt[i]!='-' && textt[i]!='/' && textt[i]!='%' && textt[i]!='^' && textt[i]!='*'){
+        while(i < textt.length && textt[i]!='+' && textt[i]!='-' && textt[i]!='/' && textt[i]!='%' && textt[i]!='^' && textt[i]!='*'){
             num1 = parseInt(num1 * 10) + parseInt(textt[i]);
             i++;
         }
